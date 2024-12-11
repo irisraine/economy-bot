714505c46f28d722e58373e356be30a434e46ef5
Iris-Raine <iris.raine.darkest.star@gmail.com>
2024-12-11 23:41:40 +0000

    enlarge limit of user balances per embed

--- engine/messages.py
+++ engine/messages.py
@@ -509,7 +509,7 @@ def bank_balance():
 
 
 def all_users_balances():
-    max_users = 50
+    max_users = 100
     embed_messages = []
     all_users_balances_list = sql.get_all_users_balances()
     number_of_users = len(all_users_balances_list)
